049dd50670d369b8574dc2206861a98acd759dd3
mx-99 <125824465+mx-99@users.noreply.github.com>
2026-02-06 15:57:37 +0300

    Refactor

--- 01_arrays/array.js
+++ 01_arrays/array.js
@@ -1,34 +1,63 @@
-//Creating Arrays
+// Array Creation & Basics
 
-let num = [];
-let num1 = [1, 2, 3, 4];
-let num2 = new Array();
-let num3 = new Array(1, 2, 3, 4, 5);
-let num4 = new Array(6);
-var obj = ["a", 2, true, null];
-const number = 2;
-console.log(num.length);
-console.log(num2.length);
-console.log(num3.length);
-console.log(obj.length);
-console.log(Array.isArray(obj));
-console.log(Array.isArray(number));
+function arrayBasics() {
+  const emptyArray = [];
+  const numbers = [1, 2, 3, 4];
+  const mixedArray = ["a", 2, true, null];
+  const singleLengthArray = new Array(6);
 
-//Accessing and Writing Array Elements
-
-let arr = [];
-for (let i = 0; i < 10; ++i) {
-  arr[i] = i + 1;
+  console.log(emptyArray.length);
+  console.log(numbers.length);
+  console.log(mixedArray.length);
+  console.log(singleLengthArray.length);
+  console.log(Array.isArray(mixedArray));
+  console.log(Array.isArray(2));
 }
-console.log(arr);
 
-const arr = [1, 2, 3];
-let sum = arr[0] + arr[1] + arr[2];
-console.log(sum);
+// arrayBasics();
 
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var sum = 0;
-for (let i = 0; i < num.length; ++i) {
-  sum += num[i];
+// Accessing & Writing Elements
+
+function writeArrayElements() {
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    arr[i] = i + 1;
+  }
+  console.log(arr);
 }
-console.log(sum);
+
+// writeArrayElements();
+
+function sumArrayElements() {
+  const arr = [1, 2, 3];
+  const sum = arr[0] + arr[1] + arr[2];
+  console.log(sum);
+}
+
+// sumArrayElements();
+
+function sumUsingLoop() {
+  const nums = [1,2,3,4,5,6,7,8,9,10];
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+
+  console.log(sum);
+}
+
+// sumUsingLoop();
+
+// Creating Arrays from Strings
+
+function createArrayFromString() {
+  const text = "Hello js world!";
+  const words = text.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    console.log(`Word ${i}: ${words[i]}`);
+  }
+}
+
+// createArrayFromString();
